1c50ac767a19b91432be5493e2c1aa7a56864629
mtyhon <34290886+mtyhon@users.noreply.github.com>
2022-11-18 16:37:06 -1000

    Update ace.py

--- ace/ace.py
+++ ace/ace.py
@@ -536,7 +536,7 @@ class ACEModel(tf.keras.Model):
                 [tf.range(num_samples), tf.repeat(j, num_samples)], axis=1
             )
 
-            curr_x_o = tf.cast(curr_x_o, tf.float32)
+            dim_samples = tf.cast(dim_samples, tf.int64)
             curr_x_o = tf.tensor_scatter_nd_update(curr_x_o, update_inds, dim_samples)
             curr_observed_mask = tf.tensor_scatter_nd_update(
                 curr_observed_mask, update_inds, tf.ones_like(dim_samples)
